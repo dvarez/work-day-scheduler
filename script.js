8f33b6d5efaea7c907cadee5036d6fb0ccf2fea6
Dietrich Varez <dietrich1899@gmail.com>
2022-04-07 15:55:31 -0700

    button

--- script.js
+++ script.js
@@ -3,10 +3,44 @@ $("#currentDay").text(dateToday);
 
 // current hour saved to a variable
 
-var currentTime = moment().format("h");
+var currentTime = moment().format("H");
 console.log(currentTime)
 
 // change past time to light-gray
 // change current time to red
 // change future time to light-green
 
+$(".input-group").each(function (index) {
+    console.log($(this).attr("id"));
+
+    if (parseInt($(this).attr("id")) < currentTime) {
+        $(this).addClass("past")
+
+
+    }
+
+    if (parseInt($(this).attr("id")) == currentTime) {
+        $(this).addClass("present")
+    }
+
+
+    if (parseInt($(this).attr("id")) > currentTime) {
+        $(this).addClass("future")
+
+    }
+
+
+
+});
+
+
+
+
+
+$( ".saveBtn" ).on( "click", function() {
+    console.log( $( this ).siblings("input").val() );
+    var time = $(this).parent().attr("id")
+    var text = $( this ).siblings("input").val()
+    localStorage.setItem(time,text)
+
+  });
