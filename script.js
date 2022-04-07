c790ee176332bc246bbf9d2ae9c53794e934b393
Dietrich Varez <dietrich1899@gmail.com>
2022-04-07 10:26:39 -0700

    blocks

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 var dateToday = moment().format("dddd, MMMM Do");
 $("#currentDay").text(dateToday);
+
+// current hour saved to a variable
+
+var currentTime = moment().format("h");
+console.log(currentTime)
+
+// change past time to light-gray
+// change current time to red
+// change future time to light-green
+
